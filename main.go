2b86d990f0369de87f2b561450c7a8fc816927b6
qbxt <10813JJ@gmail.com>
2020-11-11 09:55:56 -0700

    removed pings, send static 69 instead

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"github.com/go-ping/ping"
 	"github.com/parnurzeal/gorequest"
 	"github.com/qbxt/ping-benson-client/constants"
 	"time"
@@ -10,19 +9,9 @@ import (
 
 func main() {
 	requester := gorequest.New().Timeout(2 * time.Second)
-	pinger, err := ping.NewPinger("www.google.com")
-	if err != nil {
-		panic(err)
-	}
-	pinger.SetPrivileged(true)
 	for {
-		pinger.Count = 1
-		if err := pinger.Run(); err != nil {
-			panic(err)
-		}
-		stats := pinger.Statistics()
-		requester.Post(fmt.Sprintf(constants.URL_TEMPLATE, constants.TOKEN, stats.AvgRtt / time.Millisecond)).End(printStatus)
-		fmt.Printf("Sent heartbeat: ID %s, Ping %d\n", constants.TOKEN, stats.AvgRtt / time.Millisecond)
+		requester.Post(fmt.Sprintf(constants.URL_TEMPLATE, constants.TOKEN, 69)).End(printStatus)
+		fmt.Printf("Sent heartbeat: ID %s, Ping %d\n", constants.TOKEN, 69)
 		time.Sleep(20 * time.Second)
 	}
 }
